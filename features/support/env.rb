971c96e3592babdc10e874bc658ca94ff70717dd
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-11-24 23:11:29 +0000

    Do not enforce integrity header

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,8 @@
 # Set this explicitly
 $api_key = "12312312312312312312312312312312"
+
+AfterConfiguration do |_config|
+  MazeRunner.config.receive_no_requests_wait = 15
+  MazeRunner.config.receive_requests_wait = 15
+  MazeRunner.config.enforce_bugsnag_integrity = false
+end
